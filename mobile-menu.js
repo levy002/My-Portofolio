185926f6356d34779d2864eda39382797d3e9524
iLynette <lynetteacholah@icloud.com>
2022-01-24 15:18:51 +0300

    Add js file for functionality on the mobile popup

---
+++ mobile-menu.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.mobile-menu'); 
+const menuList = document.querySelectorAll('.mobile-menu li'); 
+const openMenu = document.querySelector('#hamburger-icon'); 
+const closeMenu = document.querySelector('#cross-arrow'); 
+const list = Array.from(menuList); 
+
+function displayMenu() { 
+    menu.style.display = 'flex';
+ } 
+
+function disappearMenu() { 
+    menu.style.display = 'none'; 
+} 
+
+openMenu.addEventListener('click', displayMenu); 
+closeMenu.addEventListener('click', disappearMenu); 
+list.forEach((link) => { link.addEventListener('click', disappearMenu); }); 
