7d7ce7310ca356f4f840a9c8529e84887d6b1553
levy002 <levy.ukwi002@gmail.com>
2022-01-27 15:34:57 +0200

    Add one javascript object to store all form data

--- storage.js
+++ storage.js
@@ -3,26 +3,27 @@ const lastName = document.getElementById('last_name');
 const useremail = document.getElementById('user_email');
 const userMessage = document.getElementById('user-message');
 
-function setStorage() {
-  const currentName = localStorage.getItem('name');
-  const currentLastName = localStorage.getItem('lastName');
-  const currentEmail = localStorage.getItem('email');
-  const currentMessage = localStorage.getItem('message');
-
-  userNames.value = currentName;
-  lastName.value = currentLastName;
-  useremail.value = currentEmail;
-  userMessage.value = currentMessage;
-}
-
 function populateStorage() {
-  localStorage.setItem('name', userNames.value);
-  localStorage.setItem('lastName', lastName.value);
-  localStorage.setItem('email', useremail.value);
-  localStorage.setItem('message', userMessage.value);
-  setStorage();
+    let formData = {
+        firstName: userNames.value,
+        lastName: lastName.value,
+        email: useremail.value,
+        message: userMessage.value
+    }
+    localStorage.setItem('userData', JSON.stringify(formData));
+    let savedFormData = localStorage.getItem('userData');
+    savedFormData = JSON.parse(savedFormData);
 }
-userMessage.onchange = populateStorage;
-useremail.onchange = populateStorage;
-lastName.onchange = populateStorage;
-userNames.onchange = populateStorage;
+
+window.onload = () => {
+    let savedFormData = localStorage.getItem('userData');
+    savedFormData = JSON.parse(savedFormData);
+    if(savedFormData !== null) {
+        userNames.value = savedFormData.firstName;
+        lastName.value = savedFormData.lastName;
+        useremail.value = savedFormData.email;
+        userMessage.value = savedFormData.message
+    }
+    populateStorage();
+}
+ 
